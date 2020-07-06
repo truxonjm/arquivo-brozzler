d71a1500356ae153493519aa4e3ae90761d76788
Daniel Bicho <daniel.bicho@fccn.pt>
2020-07-06 15:27:43 +0100

    change base SO to debian instead of ubuntu

--- brozzler-worker/xvfb-run-chromium-browser.sh
+++ brozzler-worker/xvfb-run-chromium-browser.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xvfb-run chromium-browser --no-sandbox --disable-gpu --remote-debugging-address=0.0.0.0 "$@"
+xvfb-run chromium --no-sandbox --disable-gpu --remote-debugging-address=0.0.0.0 "$@"
